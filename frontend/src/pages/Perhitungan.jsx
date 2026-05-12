069271dc30917b57a5cc5a56a14995a30aa1ecec
dwikikurniawanedu <dwik23235ti@student.nurulfikri.ac.id>
2026-05-12 17:50:31 +0700

    Update: Fix LOS - DJ

--- frontend/src/pages/Perhitungan.jsx
+++ frontend/src/pages/Perhitungan.jsx
@@ -25,7 +25,7 @@ const getTodayDateInput = () => {
 }
 
 const losColors = {
-  'A': 'bg-purple-100 text-purple-800',
+  'A': 'bg-violet-600 text-white',
   'B': 'bg-blue-100 text-blue-800',
   'C': 'bg-green-100 text-green-800',
   'D': 'bg-yellow-100 text-yellow-800',
